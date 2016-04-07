c4bc9e79bfe75c5fc497ef2427ccea836b05191b
Yize Zhang <374826671@qq.com>
2016-04-07 19:44:11 +0800

    Create 1.4.1.cpp

---
+++ 1.4.1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int main()
+{
+	int i,sum;
+	int leftI,rightI;
+	// 1.9
+	i=50;
+	while (i<=100){
+		sum+=i;
+		++i;
+	}
+	std::cout << "Sum of 50 to 100 inclusive is " << sum << std::endl;
+	//1.10
+	i=100;
+	sum=0;
+	while (i>=50){
+		sum+=i;
+		--i;
+	}
+	std::cout << "Sum of 100 to 50 inclusive is " << sum << std::endl;
+	//1.11
+	std::cout << "Please input 2 int numbers" << std::endl;
+	std::cin >> leftI >> rightI;
+	std::cout << "You have input two numbers " << leftI << " " << rightI << "\nThe nembers between them are:  " << std::endl;
+	if (leftI>rightI){
+		std::swap(leftI,rightI);
+	}
+	i=leftI;
+	while(i<=rightI){
+		std::cout << i << " ";
+		++i;
+	}
+	std::cout << std::endl;
+	return 0;
+}
