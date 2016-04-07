790e6f55a8969997e8560f8fadd5000113e441e2
Yize Zhang <374826671@qq.com>
2016-04-07 19:50:19 +0800

    Create 1.2.cpp

---
+++ 1.2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main ()
+{	
+	//1.3
+	std::cout << " Hello, Wrold." << std::endl;
+	int i,j;
+	i=3;
+	j=5;
+	//1.4
+	std::cout << i << "*" << j << "=" << (i*j) << std::endl;
+	//1.5
+	std::cout << "Hello, Wrold.\n" << i << "*" << j << "=" << (i*j) << std::endl;
+	return 0;
+}
