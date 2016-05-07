44f8fa86bdbd77e845a53174ce4f28e79dbbce59
Yize Zhang <374826671@qq.com>
2016-05-07 17:02:04 +0800

    Create Chapter13.h

---
+++ Chapter13.h
@@ -0,0 +1,105 @@
+#ifndef CHAPTER13_H
+#define CHAPTER13_H
+
+#include <set>
+#include <string>
+
+class HasPtr {
+public:
+    HasPtr(const std::string& s = std::string()) : ps(new std::string(s)), i(0)
+    {
+    }
+    HasPtr(const HasPtr& hp) : ps(new std::string(*hp.ps)), i(hp.i) {}
+    HasPtr& operator=(const HasPtr& hp)
+    {
+        std::string* new_ps = new std::string(*hp.ps);
+        delete ps;
+        ps = new_ps;
+        i = hp.i;
+        return *this;
+    }
+    ~HasPtr() { delete ps; }
+private:
+    std::string* ps;
+    int i;
+};
+
+class Folder;
+
+class Message {
+    friend void swap(Message&, Message&);
+    friend void swap(Folder&, Folder&);
+    friend class Folder;
+
+public:
+    explicit Message(const std::string& str = "") : contents(str) {}
+    Message(const Message&);
+    Message& operator=(const Message&);
+    ~Message();
+    void save(Folder&);
+    void remove(Folder&);
+
+    void print_debug();
+
+private:
+    std::string contents;
+    std::set<Folder*> folders;
+
+    void add_to_Folders(const Message&);
+    void remove_from_Folders();
+
+    void addFldr(Folder* f) { folders.insert(f); }
+    void remFldr(Folder* f) { folders.erase(f); }
+};
+
+void swap(Message&, Message&);
+
+class Folder {
+    friend void swap(Message&, Message&);
+    friend void swap(Folder&, Folder&);
+    friend class Message;
+
+public:
+    Folder() = default;
+    Folder(const Folder&);
+    Folder& operator=(const Folder&);
+    ~Folder();
+
+    void print_debug();
+
+private:
+    std::set<Message*> msgs;
+
+    void add_to_Messages(const Folder&);
+    void remove_from_Messages();
+
+    void addMsg(Message* m) { msgs.insert(m); }
+    void remMsg(Message* m) { msgs.erase(m); }
+};
+
+void swap(Folder&, Folder&);
+
+class String {
+public:
+    String() : String("") {}
+    String(const char*);
+    String(const String&);
+    String& operator=(const String&);
+    ~String();
+
+    const char* c_str() const { return elements; }
+    size_t size() const { return end - elements; }
+    size_t length() const { return end - elements - 1; }
+
+private:
+    std::pair<char*, char*> alloc_n_copy(const char*, const char*);
+    void range_initializer(const char*, const char*);
+    void free();
+
+private:
+    char* elements;
+    char* end;
+    std::allocator<char> alloc;
+};
+
+#endif
